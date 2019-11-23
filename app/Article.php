a0d32d21460edc7a1acf1ecb6887dc0b63e9b4f1
hirokichi1992 <plastar14105@gmail.com>
2019-11-23 19:38:54 +0900

    add mass asignment to article model

--- app/Article.php
+++ app/Article.php
@@ -6,5 +6,6 @@ use Illuminate\Database\Eloquent\Model;
 
 class Article extends Model
 {
-    //
+    //マスアサインメントの設定（ここに定義した変数のみ追加できる_セキュリティ対策）
+    protected $fillable = ['title','body','published_at'];
 }
